2bfef8a1faeeb62beb7e1f8f5fb8e08317556a59
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-11-22 11:06:21 -0600

    sideMenu

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss',
+})
+export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    {
+      title: 'Contador',
+      route: 'counter',
+    },
+    {
+      title: 'Usuario',
+      route: 'user-info',
+    },
+    {
+      title: 'Mutaciones',
+      route: 'properties',
+    },
+  ];
+}
--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-signals',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SideMenuComponent],
   templateUrl: './signals.component.html',
   styleUrl: './signals.component.scss',
 })
