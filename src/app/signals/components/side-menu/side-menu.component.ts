ea16af41ce5f40bfd3da42f7ecec0e5a9a55402d
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-11-22 11:15:25 -0600

    menuItem con signals

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
@@ -15,7 +15,7 @@ interface MenuItem {
   styleUrl: './side-menu.component.scss',
 })
 export class SideMenuComponent {
-  public menuItems: MenuItem[] = [
+  public menuItems = signal<MenuItem[]>([
     {
       title: 'Contador',
       route: 'counter',
@@ -28,5 +28,20 @@ export class SideMenuComponent {
       title: 'Mutaciones',
       route: 'properties',
     },
-  ];
+  ]);
+
+  // public menuItems: MenuItem[] = [
+  //   {
+  //     title: 'Contador',
+  //     route: 'counter',
+  //   },
+  //   {
+  //     title: 'Usuario',
+  //     route: 'user-info',
+  //   },
+  //   {
+  //     title: 'Mutaciones',
+  //     route: 'properties',
+  //   },
+  // ];
 }
