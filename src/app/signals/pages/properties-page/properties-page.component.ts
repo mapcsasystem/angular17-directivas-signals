15279c559ec3541b63c41808f6946cf0e6a57f1f
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-11-23 11:46:58 -0600

    mutaciones

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './properties-page.component.html',
-  styleUrl: './properties-page.component.scss'
+  styleUrl: './properties-page.component.scss',
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnInit, OnDestroy {
+  public counter = signal(10);
 
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
+
+  public fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  public userChangedEffect = effect(() => {
+    // Todo: Descomentar esta línea
+    console.log(`${this.user().first_name} - ${this.counter()} `);
+  });
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.counter.update((current) => current + 1);
+    }, 1000);
+  }
+
+  increaseBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
+
+  onFieldUpdated(field: keyof User, value: string) {
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return current;
+    });
+  }
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
 }
